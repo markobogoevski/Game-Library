69954de5a63855024d1b636fc2b1367fbafd162b
markobogoevski <markobogoevski@gmail.com>
2019-08-12 01:28:03 +0200

    update 5

--- src/app/account/sign-up.component.ts
+++ src/app/account/sign-up.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { User } from './user';
+import {NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
+  
+  submitted:boolean;
+  formInvalid:boolean;
+  private accountRepoUrl:string="app/account/accounts.json";
 
-  constructor() { }
+  user: User = {
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    adress1: '',
+    adress2: '',
+    age: 10,
+    city: '',
+    country: '',
+    emailAdress: '',
+    password: '',
+    terms: false,
+    zip: ''
+  }
+
+  countries = [
+    "United States of America", "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Deps",
+    "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+    "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
+    "Bosnia Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina", "Burma", "Burundi",
+    "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Rep", "Chad", "Chile",
+    "People's Republic of China", "Republic of China", "Colombia", "Comoros", "Democratic Republic of the Congo",
+    "Republic of the Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Danzig",
+    "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador",
+    "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France",
+    "Gabon", "Gaza Strip", "The Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala",
+    "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Holy Roman Empire", "Honduras", "Hungary", "Iceland",
+    "India", "Indonesia", "Iran", "Iraq", "Republic of Ireland", "Israel", "Italy", "Ivory Coast",
+    "Jamaica", "Japan", "Jonathanland", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "North Korea",
+    "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
+    "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia",
+    "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia",
+    "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mount Athos", "Mozambique", "Namibia", "Nauru",
+    "Nepal", "Newfoundland", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway",
+    "Oman", "Ottoman Empire", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
+    "Poland", "Portugal", "Prussia", "Qatar", "Romania", "Rome", "Russian Federation", "Rwanda, St Kitts & Nevis",
+    "St Lucia", "Saint Vincent & the, Grenadines", "Samoa", "San Marino", "Sao Tome & Principe", "Saudi Arabia",
+    "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands",
+    "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland",
+    "Syria", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey",
+    "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan",
+    "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+  ]
+
+  constructor(private http:HttpClient) { 
+    this.submitted=false;
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(): void {
+    this.submitted=false;
+  }
+
+  onSubmit(form:NgForm) {
+    this.submitted=true;
+    if(form.valid){
+    alert('Account was created!')
+    form.resetForm();
+    this.http.post<User>(this.accountRepoUrl,JSON.stringify(this.user));
+    this.submitted=false;
+    }
+  }
+
+  get diagnostic() { return JSON.stringify(this.user); }
 }
---
+++ src/app/account/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+    firstName:string;
+    lastName:string;
+    emailAdress:string;
+    password:string;
+    adress1:string;
+    adress2?:string;
+    phoneNumber?:string;
+    city:string;
+    country:string;
+    zip:string;
+    age:number;
+    terms:boolean;
+}
